c9cbb0db9bcbc4df3a5ccdad8a1153eff5c0b83d
shycubes <53842132+shycubes@users.noreply.github.com>
2019-08-07 12:00:05 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 initializeItems();
+initializeBoxes();
 
 function initializeItems() {
 
-  //Give option bar event listener
+  document.getElementById('optionButton').addEventListener('click', () => { moveBar() });
 
-  //All the boxes on the page
+  const themes = document.getElementsByClassName('themes');
+  for (let i = 0; i < themes.length; i++) {
+    themes[i].addEventListener("click", () => {
+      changeThemes(themes[i]);
+    })
+  }
+}
+
+function initializeBoxes() {
   const boxes = document.getElementsByClassName('box');
-  console.log(boxes);
-
   for (let i = 0; i < boxes.length; i++) {
 
     //Give each box an event listener
@@ -16,12 +23,26 @@ function initializeItems() {
     })
   }
 
-  const themes = document.getElementsByClassName('themes');
-  for(let i = 0; i < themes.length; i++){
-    themes[i].addEventListener("click", () => {
-      changeThemes(themes[i]);
+  //Give 'Clear' button an event listener that animates and clears boxes
+  document.getElementById('clearButton').addEventListener('click', () => {
+
+    var boxes = document.getElementById('centerWrapper').children;
+
+    console.log(boxes);
+
+    //Exit animation for boxes
+    anime({
+      targets: boxes,
+      translateY: 1000,
+      delay: anime.stagger(1000),
+      duration: 5000
     })
-  }
+
+    for(let i = 0; i < boxes.length; i++){
+      boxes[i].remove();
+    }
+  });
+  
 }
 
 //Controls the movement of the "escaping" elements
@@ -64,11 +85,11 @@ function escapeFunction(box, i) {
 
   //This detects the boundaries, gives them "padding", then reverses the movement direction if they meet the condition
   if (xCord < 0) {
-    if ((Math.abs(xCord) > measurements.distanceLeft )) {
+    if ((Math.abs(xCord) > measurements.distanceLeft)) {
       console.log("LEFT VECTOR REVERSED");
       xCord *= -1;
     }
-  } else if (xCord >  Math.abs(measurements.distanceRight) - 500) {
+  } else if (xCord > Math.abs(measurements.distanceRight) - 300) {
     console.log("RIGHT VECTOR REVERSED");
     xCord *= -1;
   }
@@ -91,16 +112,15 @@ function escapeFunction(box, i) {
     targets: box,
     translateX: (xCord),
     translateY: (yCord),
-    easing: "spring",
-    duration: 400,
-  })  
+    easing: 'easeOutCirc',
+    duration: 3000,
+  })
 }
 
 var activeBar = false;
 
-function openBar() {
-  console.log(activeBar);
-  if(activeBar == false){
+function moveBar() {
+  if (activeBar == false) {
     anime({
       targets: '#optionButton',
       translateX: -310,
@@ -110,45 +130,79 @@ function openBar() {
     document.getElementById("optionBar").style.width = "350px";
     activeBar = true;
   } else {
-    document.getElementById("optionBar").style.width = "0px";
-    anime({
-      targets: '#optionButton',
-      translateX: 0,
-      easing: "easeInOutCubic",
-      duration: 500,
-    })
-    activeBar = false;
-  } 
+    function closeBar(){
+      document.getElementById("optionBar").style.width = "0px";
+      anime({
+        targets: '#optionButton',
+        translateX: 0,
+        easing: "easeInOutCubic",
+        duration: 500,
+      })
+      activeBar = false;
+    }
+    closeBar();
+  }
+
+  document.getElementById('container').addEventListener('click', () => {
+    console.log("GOT HERE");
+    closeBar();
+  }); 
 }
 
 function changeThemes(theme) {
   var containerImage = document.getElementById('container');
-  console.log("GOT HERE")
-  console.log(theme.id);
-  switch(theme.id){
 
-    case 'themeOcean': 
-      containerImage.setAttribute("style", 
-      "background-image: url(https://66.media.tumblr.com/9f6c16cfc8e6015e04ecbedca9038757/tumblr_ojp6auodWy1ufw8o4o1_400.gif)"
+  switch (theme.id) {
+
+    case 'themeOcean':
+      containerImage.setAttribute("style",
+        "background-image: url(https://66.media.tumblr.com/9f6c16cfc8e6015e04ecbedca9038757/tumblr_ojp6auodWy1ufw8o4o1_400.gif)"
       );
-    break;
+      break;
 
     case 'themeRain':
-    containerImage.setAttribute("style", 
-      "background-image: url(http://giphygifs.s3.amazonaws.com/media/NnMH7LDpZTPZS/giphy.gif)"
+      containerImage.setAttribute("style",
+        "background-image: url(http://giphygifs.s3.amazonaws.com/media/NnMH7LDpZTPZS/giphy.gif)"
       );
-    break;
+      break;
 
     case 'themeStream':
-    containerImage.setAttribute("style",
-    "background-image: url(https://66.media.tumblr.com/4db5be7085f4d3b06abbc636192b59f4/tumblr_n9823d42by1qk9powo1_500.gif)"
-    ) 
-    break;
+      containerImage.setAttribute("style",
+        "background-image: url(https://66.media.tumblr.com/4db5be7085f4d3b06abbc636192b59f4/tumblr_n9823d42by1qk9powo1_500.gif)"
+      )
+      break;
 
     case 'themeCitySunset':
-    containerImage.setAttribute("style",
-    "background-image: url(https://media.giphy.com/media/V5qXaUBISlbTa/giphy.gif)"
-    )
-    break;
+      containerImage.setAttribute("style",
+        "background-image: url(https://media.giphy.com/media/V5qXaUBISlbTa/giphy.gif)"
+      )
+      break;
   }
+
+}
+
+function createCube() {
+
+  var newCube = document.createElement("DIV");
+  var possibleColors = [' red', ' blue', ' green', ' yellow'];
+  newCube.className = 'box';
+  newCube.className += `${possibleColors[Math.floor(Math.random() * 4)]}`;
+
+  console.log(newCube);
+  console.log();
+
+  document.getElementById('centerWrapper').appendChild(newCube);
+
+  var randomYDistance = Math.floor(Math.random() * 500) + 200;
+  var randomNegative = Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+  var randomXDistance = Math.floor(Math.random() * 500) * randomNegative;
+
+  anime({
+    targets: newCube,
+    translateY: randomYDistance,
+    translateX: randomXDistance,
+    easing: 'easeOutCirc',
+    duration: 3000,
+  })
+  initializeBoxes();
 }
